f063a081e26b68bc85c65a4085138a971e0ce38e
徐浩鑫 <xingyezn@qq.com>
2019-09-15 21:13:07 +0800

    问卷的基本问题已经解决，下一步完成问卷的页面，做好提交的判断

--- models.py
+++ models.py
@@ -70,11 +70,325 @@ class User(db.Model):
 
     # 用户地区
     county = db.Column(db.String(100),default=None, nullable=True)
-
-
-
-
 """
+class Questionnaire(db.Model):
+    __tablename__ = 'questionnaire'
+
+    # 数据id
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    # 数据所属用户的id
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), default=None, nullable=False)
+
+    # 用户角色
+    user_data = db.relationship('User', backref='data_users')
+
+    # S-F风格感官 - 感受
+    # 个人的
+    SF1 = db.Column(db.Integer,default = None,nullable=False)
+
+    # 人
+    SF2 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 即兴的
+    SF3 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 和谐
+    SF4 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 合作
+    SF5 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 与个人相关
+    SF6 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 讨论
+    SF7 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 人际互动
+    SF8 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 感受
+    SF9 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 温暖
+    SF10 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 直觉
+    SF11 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 人性的
+    SF12 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 融洽
+    SF13 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 说服
+    SF14 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 建立关系
+    SF15 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 社会化
+    SF16 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 同情的
+    SF17 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 亲密
+    SF18 = db.Column(db.Integer,default = None,nullable=False)
+
+    # 忠诚
+    SF19 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 经验的
+    SF20 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 依恋
+    SF21 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 移情
+    SF22 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 仿效
+    SF23 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 经验
+    SF24 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 社交的
+    SF25 = db.Column(db.Integer, default=None, nullable=False)
+
+    # S-T风格感官-思考
+    # 组织的
+    ST1 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 事实
+    ST2 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 字面的
+    ST3 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 功用
+    ST4 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 竞争
+    ST5 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 记忆
+    ST6 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 指导
+    ST7 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 细节
+    ST8 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 目标
+    ST9 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 行为
+    ST10 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 尝试错误
+    ST11 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 现实的
+    ST12 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 特性
+    ST13 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 精确
+    ST14 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 知道
+    ST15 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 程序化
+    ST16 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 实际的
+    ST17 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 信息
+    ST18 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 规则
+    ST19 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 有秩序的
+    ST20 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 正确
+    ST21 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 明确
+    ST22 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 范例
+    ST23 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 努力
+    ST24 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 循序渐进的
+    ST25 = db.Column(db.Integer, default=None, nullable=False)
+
+    # N-T风格直觉-思考
+    # 分析的
+    NT1 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 公式
+    NT2 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 解析的
+    NT3 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 问题
+    NT4 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 批判
+    NT5 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 理由
+    NT6 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 辩论
+    NT7 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 规律
+    NT8 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 想法
+    NT9 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 智慧
+    NT10 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 策略
+    NT11 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 理论的
+    NT12 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 概念
+    NT13 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 逻辑
+    NT14 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 理解
+    NT15 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 系统化
+    NT16 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 智力的
+    NT17 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 询问
+    NT18 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 原则
+    NT19 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 逻辑的
+    NT20 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 论证
+    NT21 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 好奇
+    NT22 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 解释
+    NT23 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 测试
+    NT24 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 科学的
+    NT25 = db.Column(db.Integer, default=None, nullable=False)
+
+    # N-F风格直觉-感受
+    # 创造性的
+    NF1 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 热情
+    NF2 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 有弹性的
+    NF3 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 想象
+    NF4 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 创造
+    NF5 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 重新组织
+    NF6 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 发现
+    NF7 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 可能性
+    NF8 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 洞察力
+    NF9 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 怀疑
+    NF10 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 有了！
+    NF11 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 唯美的
+    NF12 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 价值
+    NF13 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 预测
+    NF14 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 表达
+    NF15 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 理想化
+    NF16 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 理想的
+    NF17 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 发明
+    NF18 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 隐喻
+    NF19 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 启示的
+    NF20 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 取舍
+    NF21 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 独创性
+    NF22 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 推断
+    NF23 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 热情
+    NF24 = db.Column(db.Integer, default=None, nullable=False)
+
+    # 对称的
+    NF25 = db.Column(db.Integer, default=None, nullable=False)
+
+
+
 class Data(db.Model):
     # 数据表格
     __tablename__ = 'data'
---
+++ static/js/index.js
@@ -0,0 +1,50 @@
+
+ function clicks(q_num,num){
+	var my_select = document.getElementById("select"+ q_num + "_" + num);
+	var my_button = document.getElementById("value"+ q_num + "_" + num);
+	var all_num = [5,3,1,0]
+	value = 0
+	for(i=1;i<=4;i++){
+		var one_select = document.getElementById("select"+ q_num+ "_" +i);
+		if(one_select.style.display == 'block'){
+			value += 1;
+		}
+	}
+	if (value == 4){
+		return 0;
+	}
+	my_select.style.display = 'block';
+	my_select.innerHTML = "<span class=\"inner\">"+ all_num[value] + "</span>";
+	my_button.value = all_num[value];
+	//console.log('选中');
+	if (value==2){
+		for(i=1;i<=4;i++){
+		var one_select = document.getElementById("select"+ q_num+ "_" +i);
+		if(one_select.style.display == 'none'){
+			one_select.style.display = 'block';
+			one_select.innerHTML = "<span class=\"inner\">"+ all_num[value+1] + "</span>";
+			var one_button = document.getElementById("value"+ q_num + "_" + i);
+			one_button.value = all_num[value+1];
+		}
+	}
+	var select0 = document.getElementById("select"+q_num+"_0");
+		select0.style.display = 'block';
+		console.log('test')
+	}
+ }
+ 
+ function myclear(q_num){
+	console.log('清除');
+	 for(i=1;i<=4;i++){
+		 var clear_select = document.getElementById("select"+ q_num + "_" +i);
+		 clear_select.style.display = 'none';
+		 var my_button = document.getElementById("value"+ q_num + "_" + i);
+		 my_button.value = null;
+	 }
+	 var select0 = document.getElementById("select"+q_num+"_0");
+	 select0.style.display = 'none';
+ }
+
+function verify() {
+alert('cuowu')
+}
